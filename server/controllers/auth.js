4f2dfb824d6a35672f2c3d2642bc9b1ff3332c29
Elena Hickey <subhickeyelena@gmail.com>
2017-07-19 16:40:37 -0400

    heroku added

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -4,41 +4,43 @@ var config = require("../config");
 
 function createUserToken(user) {
 	var timestamp = new Date().getTime();
-	return jwt.encode({ sub: user.id }, config.secret);
+	return jwt.encode({ sub: user.id, iat: timestamp}, config.secret);
 }
 
 exports.signin = function(req, res, next) {
-	// User has already had their email and pw authed, we just need to give them a token
+
+///is omitted after signup & email - now this creates user token
+
 	res.send({ token: createUserToken(req.user) });
 };
 
 exports.signup = function(req, res, next) {
-	//1
+//1
 	var email = req.body.email;
 	var password = req.body.password;
 
 	if( !email || !password) {
 		return res.status(418).send({error: "Email and password must be provided"});
 	}
-	//2
+//2
 	User.findOne({ email: email }, function(err, existingUser) {
 		if(err) {
 			return next(err);
-		//return search error
+	//return search error
 		}
 		if(existingUser) {
 			// return res.status(418).send(err); 
-			return res.status(418).send("Account with that email has already been created");
+			return res.status(418).send("An Account with that email has already been created");
 			// handles existing users
 		}
 	});
 
-	// 3
+// 3
 	var user = new User({
 		email: email,
 		password: password
 	});
-	// to save the record to the DB
+// to save the record to the DB
 	user.save(function(err) {
 		if(err) { 
 			return next(err); 
